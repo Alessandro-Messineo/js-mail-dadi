e9761bb5b5fe887a37c7b95ac6a261f7f00cd762
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-01 13:54:06 +0200

    first commit

---
+++ es mail/js/script.js
@@ -0,0 +1,17 @@
+// Crea una lista di email di invitati ad una festa.
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+
+const arrEmail = [
+  "mario.rossi@fakeemail.com",
+  "lucia.bianchi@fakeemail.com",
+  "giovanni.verdi@fakeemail.com",
+  "anna.neri@fakeemail.com",
+  "paolo.costa@fakeemail.com",
+  "francesca.rizzo@fakeemail.com",
+  "luca.fontana@fakeemail.com",
+  "chiara.gallo@fakeemail.com",
+  "andrea.moretto@fakeemail.com",
+  "elisa.ferri@fakeemail.com"
+];
